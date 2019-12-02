bf5153064f502da8f7e93ff6fba8d3c88e235f4b
swyx <shawnthe1@gmail.com>
2019-12-02 13:12:29 -0500

    update notifier

--- cli/packages/mycli/src/base.ts
+++ cli/packages/mycli/src/base.ts
@@ -2,13 +2,22 @@ import { Command } from "@oclif/command";
 const { cosmiconfig } = require("cosmiconfig");
 const explorer = cosmiconfig("mycli");
 var debug = require("debug")("mycli:base");
-
 type ConfigType = {
   name?: string;
 };
+
+const updateNotifier = require("update-notifier");
+const pkg = require("../package.json");
+
 export default abstract class Base extends Command {
   static config: null | ConfigType;
   async init() {
+    const notifier = updateNotifier({
+      pkg,
+      updateCheckInterval: 1000,
+      shouldNotifyInNpmScript: true
+    });
+    notifier.notify();
     const { config, filepath } = (await explorer.search()) || {};
     debug("parsing config", { config, filepath });
     this.config = config;
